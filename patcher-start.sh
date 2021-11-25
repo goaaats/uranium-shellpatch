29ea3bc66d27b888fb28848f2ed475afb6267ae4
minhnguyenhoang <mr.hatdau@gmail.com>
2021-11-25 17:20:56 +0700

    IPv6 bug fix v2
    Still doesn't work, committing this then testing

--- patcher-start.sh
+++ patcher-start.sh
@@ -143,9 +143,9 @@ if [ "$(${abs} -m pip freeze | grep requests)" == "" ]; then
   echo "${TEXT_WARN}Attempting to install Requests...${TEXT_NONE}"
   sudo -H ${abs} -m pip install requests
 fi
-echo "Disabling IPv6 could improve patching speed. Do you want to disable IPv6?"
-read ipv6yesno
-if ["$ipv6yesno" = "y"] || ["$ipv6yesno" = "yes"] || ["$ipv6yesno" = "Y"] || ["$ipv6yesno" = "Yes"]; then
+
+read -p "Disabling IPv6 could improve patching speed. Do you want to disable IPv6? [y/n]: " ipv6yesno
+if [["$ipv6yesno" == "y" || "$ipv6yesno" == "yes" || "$ipv6yesno" == "Y" || "$ipv6yesno" == "Yes" || "$ipv6yesno" == "YES"]]; then
   sudo sysctl net.ipv6.conf.all.disable_ipv6=1
   echo "IPv6 has been disabled."
 else
